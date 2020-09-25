fb266f0e4faeef9820e5f71cc1a670d12714dd31
Sarka Chwastkova <chwastkova.s@gmail.com>
2020-09-25 13:07:52 +0200

    feature: parallax, responsive styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,13 +4,30 @@ type Props = {
   children: React.ReactNode
 }
 
-const Layout = ({ children }: Props) => (
-  <div className="wrapper">
-    <div className="container">
-      {children}
-    </div>
-    <div className="circle"></div>
-  </div>
-)
+const Layout = ({ children }: Props) => {
+    const parallax = (e) => {
+        const elem = document.querySelector(".dots");
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        elem.style.backgroundPosition = x;
+    }
+    document.addEventListener("mousemove", parallax);
+
+    return (
+      <div className="wrapper">
+        <div className="dots"></div>
+        <div className="container">
+          {children}
+        </div>
+        <div className="circle"></div>
+      </div>
+    )
+}
 
 export default Layout
